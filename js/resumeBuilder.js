3e1360a65dcf799c08fd173bb3c6907633e05bb8
Sylvie Fiquet <sfiquetdev@gmail.com>
2015-11-07 17:52:41 +0000

    Lesson 1 var quiz: define a variable and log it in the console

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,4 +2,6 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
  $("#main").append("Sylvie Fiquet");
- 
+
+ var awesomeThought = "I am Sylvie and I am AWESOME!";
+ console.log(awesomeThought);

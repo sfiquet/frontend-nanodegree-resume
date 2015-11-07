0e726009b568468873e9350ff5d345c904be86cd
Sylvie Fiquet <sfiquetdev@gmail.com>
2015-11-07 21:11:45 +0000

    Lesson 1 Biography quiz: define a biography object and display it on the page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,11 +2,30 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
-var name = "Sylvie Fiquet";
-var role = "Web Developer";
+var bio = {
+	"name": "Sylvie Fiquet",
+	"role": "Web Developer",
+	"contact": "sfiquetdev@gmail.com",
+	"picture": "images/fry.jpg",
+	"welcome": "Welcome to my Web Page!",
+	"skills": ["Programming", "Javascript", "Node.js", "Python", "TDD", "Git"]
+};
 
-var formattedName = HTMLheaderName.replace("%data%", name);
-var formattedRole = HTMLheaderRole.replace("%data%", role);
+var formatted = {
+	"name": HTMLheaderName.replace("%data%", bio.name),
+	"role": HTMLheaderRole.replace("%data%", bio.role),
+	"contact": HTMLemail.replace("%data%", bio.contact),
+	"picture": HTMLbioPic.replace("%data%", bio.picture),
+	"welcome": HTMLwelcomeMsg.replace("%data%", bio.welcome),
+	"skills": HTMLskills.replace("%data%", bio.skills.join(", "))
+};
 
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);
+$("#header").prepend(formatted.role);
+$("#header").prepend(formatted.name);
+$("#header").prepend(formatted.picture);
+
+$("#header").append(formatted.welcome);
+$("#header").append(formatted.skills);
+$("#footerContacts").append(formatted.contact);
+
+

a51da0568b99de9264dbd947de4680730b225c7a
Sylvie Fiquet <sfiquetdev@gmail.com>
2015-11-07 18:07:37 +0000

    Lesson 1 string.replace() quiz: replace AWESOME with FUN in string and .append() to the page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Sylvie Fiquet");
+$("#main").append("Sylvie Fiquet");
+$("#main").append("<br>");
 
- var awesomeThought = "I am Sylvie and I am AWESOME!";
- console.log(awesomeThought);
+var awesomeThought = "I am Sylvie and I am AWESOME!";
+console.log(awesomeThought);
+
+var funThought = awesomeThought.replace("AWESOME", "FUN");
+$("#main").append(funThought);

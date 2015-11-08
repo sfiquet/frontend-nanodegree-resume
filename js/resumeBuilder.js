0022a5a3a91495a451159da745d65020bb4530b4
Sylvie Fiquet <sfiquetdev@gmail.com>
2015-11-08 21:43:57 +0000

    Lesson 1 All the Resume Sections: create all the objects for the resume and move the existing code into a new file

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,54 +5,104 @@ This is empty on purpose! Your code to build the resume will go here.
 var bio = {
 	"name": "Sylvie Fiquet",
 	"role": "Web Developer",
-	"contact": {
-		"mobile": "01234-1234",
+	"welcomeMessage": "Welcome to my Web Page!",
+	"contacts": {
+		"mobile": "",
 		"email": "sfiquetdev@gmail.com",
 		"github": "sfiquet",
-		"twitter": "bla",
+		"twitter": "",
 		"location": "United Kingdom"
 	},
-	"picture": "images/fry.jpg",
-	"welcome": "Welcome to my Web Page!",
-	"skills": ["Programming", "Javascript", "Node.js", "Python", "TDD", "Git"]
+	"skills": [ "Programming", "Javascript", "Node.js", "Python", "TDD", "Git" ]
 };
 
-var work = {};
-work.position = "Computer Science Teacher";
-work.employer = "Priory Group";
-work.years = "2012-2013";
-work.city = "Godalming";
-
-var education = {};
-education["name"] = "University René Descartes Paris V";
-education["years"] = "1990-1991";
-education["city"] = "Paris, France";
-
-
-var formatted = {
-	"name": HTMLheaderName.replace("%data%", bio.name),
-	"role": HTMLheaderRole.replace("%data%", bio.role),
-	"contact": HTMLemail.replace("%data%", bio.contact.email),
-	"picture": HTMLbioPic.replace("%data%", bio.picture),
-	"welcome": HTMLwelcomeMsg.replace("%data%", bio.welcome),
-	"skills": HTMLskills.replace("%data%", bio.skills.join(", "))
+var work = {
+	"jobs": [
+		{
+			"employer": "Priory Group",
+			"title": "Computer Science Teacher",
+			"location": "Godalming, UK",
+			"dates": "2012-2013",
+			"description": "Taught GCSE Computing to a very small class of autistic students with Python as the main programming language"
+		},
+		{
+			"employer": "Software Answers Ltd",
+			"title": "Software Engineer",
+			"location": "Farnborough, UK",
+			"dates": "1994-1999",
+			"description": "Developed desktop software for Windows in the domains of sales forecasting and analysis and supply chain management. Design, coding, testing, maintenance, documentation, communication with QA and trainers"
+		},
+		{
+			"employer": "Banque de France",
+			"title": "Contractor",
+			"location": "Marne-la-Vallée, France",
+			"dates": "1992-1993",
+			"description": "Maintain, extend and document expert system written in C, liaise with deployment team and with knowledge engineer. Prototyped migration from DOS to Windows."
+		},
+		{
+			"employer": "Société Générale",
+			"title": "Contractor",
+			"location": "Paris",
+			"dates": "1991-1992",
+			"description": "Wrote a UI generation tool for options trading applications."
+		}
+	]
 };
 
-$("#header").prepend(formatted.role);
-$("#header").prepend(formatted.name);
-$("#header").prepend(formatted.picture);
+var projects = {
+	"projects": [
+		{
+			"title": "Monster Workshop",
+			"dates": "2015-ongoing",
+			"description": "Monster customisation utility for Pathfinder game masters, written with Node.js and Express",
+			"images": ["example.com/image1", "example.com/image2"]
+		}
+	]
+};
 
-$("#header").append(formatted.welcome);
-$("#header").append(formatted.skills);
-$("#footerContacts").append(formatted.contact);
-
-$("#workExperience").append(HTMLworkStart);
-$("#workExperience").append(HTMLworkEmployer.replace("%data%", work["employer"]));
-$("#workExperience").append(HTMLworkTitle.replace("%data%", work["position"]));
-$("#workExperience").append(HTMLworkDates.replace("%data%", work["years"]));
-$("#workExperience").append(HTMLworkLocation.replace("%data%", work["city"]));
-
-$("#education").append(HTMLschoolStart);
-$("#education").append(HTMLschoolName.replace("%data%", education.name));
-$("#education").append(HTMLschoolDates.replace("%data%", education.years));
-$("#education").append(HTMLschoolLocation.replace("%data%", education.city));
+var education = {
+	"schools": [
+		{
+			"name": "University René Descartes Paris V",
+			"location": "Paris, France",
+			"degree": "Master (DESS)",
+			"majors": ["Computer Science"],
+			"dates": "1990-1991",
+			"url": "https://www.univ-paris5.fr/eng"
+		},
+		{
+			"name": "University Rennes I, Faculté de Médecine",
+			"location": "Rennes, France",
+			"degree": "Moved to Computer Science after 5 years so did not graduate as a MD",
+			"majors": ["Medicine"],
+			"dates": "1985-1990",
+			"url": "http://www.medecine.univ-rennes1.fr"
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Intro to Computer Science",
+			"school": "Udacity",
+			"dates": 2013,
+			"url": "https://www.udacity.com/course/intro-to-computer-science--cs101"
+		},
+		{
+			"title": "Algorithms, Part I",
+			"school": "Coursera",
+			"dates": 2013,
+			"url": "https://www.coursera.org/course/algs4partI"
+		},
+		{
+			"title": "Algorithms, Part II",
+			"school": "Coursera",
+			"dates": 2013,
+			"url": "https://www.coursera.org/course/algs4partII"
+		},
+		{
+			"title": "Intro to HTML and CSS",
+			"school": "Udacity",
+			"dates": 2015,
+			"url": "https://www.udacity.com/course/intro-to-html-and-css--ud304"
+		}
+	]
+};
